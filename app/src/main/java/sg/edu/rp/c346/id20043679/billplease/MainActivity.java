419ce72fe1e1d4108eb72a3329d11b492fe88b8a
LowHongTeck <20043679@myrp.edu.sg>
2021-05-07 19:26:50 +0800

    MainActivity

--- app/src/main/java/sg/edu/rp/c346/id20043679/billplease/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20043679/billplease/MainActivity.java
@@ -3,21 +3,30 @@ package sg.edu.rp.c346.id20043679.billplease;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText amount;
     EditText pax;
     EditText discount;
-    RadioGroup tax;
+    RadioGroup rgPayment;
     Button split;
     Button reset;
     TextView bill;
     TextView pays;
+    ToggleButton svs;
+    ToggleButton gst;
+
+    double totalAmount = 0.0;
+    double tax = 0.0;
+    double totalDiscount = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,24 +40,101 @@ public class MainActivity extends AppCompatActivity {
         reset = findViewById(R.id.reset);
         bill = findViewById(R.id.totalBillText);
         pays = findViewById(R.id.eachPaysText);
-        tax = findViewById(R.id.tax);
-        double totalAmount = 0.0;
+        rgPayment = findViewById(R.id.payment);
+        svs = findViewById(R.id.toggleSVS);
+        gst = findViewById(R.id.toggleGST);
 
-        String strAmount = amount.getText().toString();
-        double dbAmount = Double.parseDouble(strAmount);
+//        amount.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String strAmount = amount.getText().toString();
+//                double dbAmount = Double.parseDouble(strAmount);
+//
+//                String billMsg = "Total Bill: $" + strAmount;
+//                bill.setText(billMsg);
+//
+//            }
+//        });
 
-        String strPax = pax.getText().toString();
-        int intPax = Integer.parseInt(strPax);
+//        pax.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                String strAmount = amount.getText().toString();
+//                double dbAmount = Double.parseDouble(strAmount);
+//
+//                String strPax = pax.getText().toString();
+//                int intPax = Integer.parseInt(strPax);
+//
+//                totalAmount = dbAmount/intPax;
+//
+//                String billMsg = "Total Bill: $" + totalAmount;
+//                bill.setText(billMsg);
+//
+//            }
+//        });
 
-        String strDiscount = discount.getText().toString();
-        int intDiscount = Integer.parseInt(strDiscount);
+        split.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String strAmount = amount.getText().toString();
+                double dbAmount = Double.parseDouble(strAmount);
 
-        String strBill = bill.getText().toString();
-        String strPays = pays.getText().toString();
+                String strPax = pax.getText().toString();
+                int intPax = Integer.parseInt(strPax);
+
+                String strDiscount = discount.getText().toString();
+                double dbDiscount = Integer.parseInt(strDiscount);
+
+                double taxAmount = 0.0;
+
+                totalAmount = dbAmount/intPax;
+
+                if (svs.isChecked()){
+                    tax += 0.1;
+                }
+                if (gst.isChecked()){
+                    tax += 0.07;
+                }
+
+                taxAmount = dbAmount*(1+tax);
+
+                totalAmount = taxAmount/intPax;
+
+                boolean haveDiscount = false;
+
+                if (dbDiscount >=1 && dbDiscount <=100.0) {
+                    haveDiscount = true;
+                }
+
+                if (haveDiscount = true){
+                    totalDiscount = totalAmount*(dbDiscount/100);
+                }
+
+                totalAmount = totalAmount - totalDiscount ;
 
 
+                String billMsg = "Total Bill: $" + totalAmount;
+                bill.setText(billMsg);
 
 
+            }
+        });
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                amount.setText("");
+                totalAmount = 0;
+                pax.setText("");
+                discount.setText("");
+                tax = 0;
+
+                svs.setChecked(false);
+                gst.setChecked(false);
+
+            }
+        });
 
 
     }
